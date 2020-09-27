c6b2a509dc813d14aec22c14f9ad70830adeda9e
Md. Samshad Rahman <samshad.ru@gmail.com>
2020-09-27 16:31:15 +0600

    test

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import db
 
 class AdmobApi:
     def __init__(self):
-        self.arr_csv = []
+        self.api_data = []
 
         with open("Auth/configs.json") as f:
             self.configs = json.load(f)
@@ -29,16 +29,16 @@ class AdmobApi:
         http = credentials.authorize(http=build_http())
         self.admob = discovery.build(name, version, http=http)
 
-    def get_report(self):
-        date_range = {'startDate': {'year': 2020, 'month': 9, 'day': 19},
-                      'endDate': {'year': 2020, 'month': 9, 'day': 20}}
-        """sort_conditions = {'dimension': 'DATE', 'order': 'DESCENDING'}
-        dimension_filters = {
-            'dimension': 'COUNTRY',
-            'matchesAny': {
-                'values': ['BD']
-            }
-        }"""
+    def get_report(self, start_date, end_date):
+        start_year = int(start_date.split('-')[0])
+        start_month = int(start_date.split('-')[1])
+        start_day = int(start_date.split('-')[2])
+        end_year = int(end_date.split('-')[0])
+        end_month = int(end_date.split('-')[1])
+        end_day = int(end_date.split('-')[2])
+
+        date_range = {'startDate': {'year': start_year, 'month': start_month, 'day': start_day},
+                      'endDate': {'year': end_year, 'month': end_month, 'day': end_day}}
         dimensions = ['DATE', 'APP', 'PLATFORM', 'COUNTRY', 'AD_UNIT', 'FORMAT']
         metrics = ['ESTIMATED_EARNINGS', 'CLICKS', 'IMPRESSIONS', 'MATCHED_REQUESTS', 'MATCH_RATE', 'SHOW_RATE']
         sort_conditions = {'dimension': 'DATE', 'order': 'DESCENDING'}
@@ -83,10 +83,10 @@ class AdmobApi:
                 arr = dimensions
                 for i in metrics:
                     arr.append(i)
-                self.arr_csv.append(arr)
+                self.api_data.append(arr)
 
     def write_csv(self):
-        df = pd.DataFrame(self.arr_csv,
+        df = pd.DataFrame(self.api_data,
                           columns=['date_formated', 'app', 'country', 'platform', 'ad_format', 'ad_unit',
                                    'estimated_earnings', 'clicks', 'impressions', 'match_request',
                                    'match_rate', 'show_rate'])
@@ -96,7 +96,7 @@ class AdmobApi:
         dbObj = db.MySql()
         dbObj.drop_table()
         dbObj.create_table()
-        for arr in self.arr_csv:
+        for arr in self.api_data:
             dbObj.insert_data([arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9], arr[10],
                                arr[11], "admob"])
         dbObj.print_table()
@@ -104,8 +104,7 @@ class AdmobApi:
 
 if __name__ == '__main__':
     test = AdmobApi()
-    report = test.get_report()
-    # print(type(report), len(report))
+    report_json = test.get_report("2020-09-22", "2020-09-23")
 
-    test.parse_data(report)
+    test.parse_data(report_json)
     test.insert_db()
